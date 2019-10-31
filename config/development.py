6754b051324cf577c9378894157da024c417d1c7
slavko_dzulovic <dzulovicslavko@gmail.com>
2019-10-31 15:51:16 +0100

    link with Heroku Postgres

--- config/development.py
+++ config/development.py
@@ -9,4 +9,7 @@ class Development(Config):
     DB_PASSWD = 'pitajpedju'
     DB_HOST = '127.0.0.1'
     DB_PORT = 5432
-    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    # SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    SQLALCHEMY_DATABASE_URI = "postgres://skmcsgcshgphki:\
+    aa4f305ba603150397bc0594625510510feb2c6cfb389250a7438b680e50bc13@ec2-54-217-221-21.\
+    eu-west-1.compute.amazonaws.com:5432/dg5pgitsgt4qe"
